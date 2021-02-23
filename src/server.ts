3137c7e84e48ab5da3593fc8efee45fd393f84f9
Guilherme Santos <gui_t900@hotmail.com>
2021-02-23 14:33:12 -0300

    Segundo dia

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
+import 'reflect-metadata';
 import express from 'express';
-
+import './database';
+import { router } from './routes';
 
 const app = express();
 
 
-app.get("/", (req,res) => res.json({message: "Hello World - NLW04"}));
+app.use(express.json());
+app.use(router);
 
-app.post("/", (req,res) => res.json({message: "Os dados foram salvos com sucesso!"}))
 
 app.listen(3333, () => console.log("Server is running"));
